e958d0fb08d5c95ba06a29c42dbc3d28d5cdaef4
梁钊豪 <337944401@qq.com>
2021-07-26 11:50:30 +0800

    如果是删除的话，直接发送配置

--- logagent/etcd/etcd.go
+++ logagent/etcd/etcd.go
@@ -56,11 +56,17 @@ func GetConf(key string) (collectEntryList []common.CollectEntry, err error) {
 func WatchConf(key string) {
 	for {
 		watchChan := client.Watch(context.Background(), key)
-		var newConf []common.CollectEntry
 		for wresp := range watchChan {
 			logrus.Info("get new conf etcd!")
 			for _, evt := range wresp.Events {
 				fmt.Printf("type:%s key:%s value:%s\n", evt.Type, evt.Kv.Key, evt.Kv.Value)
+				var newConf []common.CollectEntry
+				if evt.Type == clientv3.EventTypeDelete {
+					//如果是删除
+					logrus.Warningf("etcd delete the key !!!")
+					tailfile.SendNewConf(newConf)
+					continue
+				}
 				err := json.Unmarshal(evt.Kv.Value, &newConf)
 				if err != nil {
 					logrus.Errorf("json unmarshal new conf failed, err:%v", err)
